55da98b2d8abca84425623524d71a986d01a570a
AshishShilvant <ashish.shilvant@gmail.com>
2021-08-15 14:50:33 -0400

    this is commit from GitDemo

---
+++ src/test/java/MavenPractise/MavenJava/stepOne.java
@@ -0,0 +1,12 @@
+package MavenPractise.MavenJava;
+
+public class stepOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is new code from GitDemo");
+
+	}
+
+}
